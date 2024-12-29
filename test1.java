4c6e2c3d5a8669e7d674dca292a7b934c629cb1c
prasad <prasadnaiduboddu081@gmail.com>
2024-12-29 15:02:57 +0530

    test3.java crated and test1.java is modified

--- test1.java
+++ test1.java
@@ -24,5 +24,10 @@ fun saveStake(stake: Stake): CommonResponse<Stake> {
         )
     }
 }
+data class CommonResponse<T>(
+    val success: Boolean,
+    val message: String,
+    val data: T?
+)
 
 

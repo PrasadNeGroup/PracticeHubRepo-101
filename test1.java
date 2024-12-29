44122d5256e61d575a7223379ba036b610bef17e
prasad <prasadnaiduboddu081@gmail.com>
2024-12-29 14:40:03 +0530

    test1.java is modified statement is added

--- test1.java
+++ test1.java
@@ -1,5 +1,6 @@
 fun stakeConfig(dto: StakeConfigDto): CommonResponse<StakeConfigResponse> {
     println("I love Kotlin :)")
     val stakeConfigResponse = StakeConfigResponse() // Corrected variable name
+    println("StakeConfigResponse object is created!!")
     return CommonResponse(data = stakeConfigResponse) // Wrapping response in CommonResponse
 }
